ea4218af5e17620b5b97bda10ef178a21daddd8f
chen.haviv <chen.haviv@mail.huji.ac.il>
2020-08-21 00:04:05 +0300

    Added more words to english.csv

--- app/src/main/java/com/example/quickvocab/MainActivity.kt
+++ app/src/main/java/com/example/quickvocab/MainActivity.kt
@@ -2,17 +2,8 @@ package com.example.quickvocab
 
 import android.content.Intent
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.support.v7.app.AppCompatActivity
-import android.view.KeyEvent
 import android.widget.Button
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStream
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,15 +14,16 @@ class MainActivity : AppCompatActivity() {
         val engButton = findViewById<Button>(R.id.button)
         val arabButton = findViewById<Button>(R.id.button2)
         val hebButton = findViewById<Button>(R.id.button3)
+
         engButton.setOnClickListener() { // what happens when you click on English
             val intent = Intent(this, English::class.java)
             startActivity(intent)
         }
-        arabButton.setOnClickListener() { // what happens when you click on English
+        arabButton.setOnClickListener() { // what happens when you click on Arabic
             val intent = Intent(this, Arabic::class.java)
             startActivity(intent)
         }
-        hebButton.setOnClickListener() { // what happens when you click on English
+        hebButton.setOnClickListener() { // what happens when you click on Hebrew
             val intent = Intent(this, Hebrew::class.java)
             startActivity(intent)
         }
